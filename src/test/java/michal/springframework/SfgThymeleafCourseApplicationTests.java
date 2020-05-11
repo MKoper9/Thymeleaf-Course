62d7b08164e83bbdaa272f9eda2ac8ab99ade8dd
Michal Koperski <mkoperpno@gmail.com>
2020-05-11 23:11:56 +0200

    first commit

---
+++ src/test/java/michal/springframework/SfgThymeleafCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package michal.springframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgThymeleafCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
