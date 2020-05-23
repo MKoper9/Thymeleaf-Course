9d4c31c2b79456252be7d6e48f097ad2f17f91c7
Michal Koperski <mkoperpno@gmail.com>
2020-05-23 21:52:41 +0200

    create login controller and html file

---
+++ src/main/java/michal/springframework/controllers/LoginController.java
@@ -0,0 +1,33 @@
+package michal.springframework.controllers;
+
+import michal.springframework.commands.LoginCommand;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login(Model model){
+
+        model.addAttribute("loginCommand", new LoginCommand());
+
+        return "loginform";
+    }
+
+    @PostMapping("/dologin")
+    public String doLogin(@Valid LoginCommand loginCommand, BindingResult bindingResult){
+
+        if(bindingResult.hasErrors()){
+            return "loginform";
+        }
+        return "redirect:/";
+    }
+
+}
